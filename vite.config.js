4587bac25e193ebbe5601170315c23f7a493d3c1
samkumar889 <sameerkumar88977@gmail.com>
2026-05-14 22:43:06 +0530

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -18,24 +17,3 @@ export default defineConfig({
     }
   }
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
-})
->>>>>>> ae51c7e034c705837097d75c87f05cfd1ebbec29
